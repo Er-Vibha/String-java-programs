ea343544b4a3d48cc9f4571973e40f3fb9127833
Vibha <65108188+Er-Vibha@users.noreply.github.com>
2023-10-25 15:36:45 +0530

    Create TestStr.java

---
+++ src/TestStr.java
@@ -0,0 +1,15 @@
+//String method examples
+public class TestStr {
+    public static void main(String[] args) {
+        String name="Vibha";
+        System.out.println(name.length());
+        System.out.println(name.substring(1,4));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.charAt(3));
+        System.out.println(name.indexOf('i'));
+      int a=10;
+      float b=5.6523f;
+        System.out.printf("the int is %d and float is %7.2f", a,b);
+        System.out.format("the int is %d and float is %f", a,b);
+    }}
